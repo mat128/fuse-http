bcbaffe51827b67cd6e3cf700f797a2e0c88bb21
Mathieu Mitchell <mmitchell@iweb.com>
2016-07-08 12:13:07 -0400

    Code cleanup

--- fuse-http.py
+++ fuse-http.py
@@ -17,34 +17,29 @@ import requests
 
 class HttpFilesystem(LoggingMixIn, Operations):
     def __init__(self, base_url):
-      self.base_url = base_url
-      super(HttpFilesystem, self).__init__()
+        self.base_url = base_url
+        super(HttpFilesystem, self).__init__()
 
     def read(self, path, size, offset, fh):
-      response = requests.get(self.base_url + path)
-      value = response.content[offset:offset+size]
-      return value
+        response = requests.get(self.base_url + path)
+        return response.content[offset:offset+size]
 
     def getattr(self, path, fh=None):
-      response = requests.head(self.base_url + path)
-      if response.status_code != 200:
-        raise FuseOSError(ENOENT)
-      size = int(response.headers['content-length'])
-      try:
-        mtime = mktime(strptime(response.headers['last-modified'],
-                                "%a, %d %b %Y %H:%M:%S GMT"))
-      except KeyError:
-        mtime=time()
+        response = requests.head(self.base_url + path)
+        if response.status_code != 200:
+            raise FuseOSError(ENOENT)
+        size = int(response.headers['content-length'])
+        try:
+            mtime = mktime(strptime(response.headers['last-modified'],
+                                    "%a, %d %b %Y %H:%M:%S GMT"))
+        except KeyError:
+            mtime = time()
 
-      if path.endswith('/'): #directories
-        value = dict(st_mode=(S_IFDIR | 0o755), st_ctime=mtime,
-                    st_mtime=mtime, st_atime=mtime, st_nlink=2)
-      else:
-        value = dict(st_mode=(S_IFREG), st_nlink=1,
-                     st_size=size, st_ctime=mtime, st_mtime=mtime,
-                     st_atime=time())
+        if path.endswith('/'):
+            return dict(st_mode=(S_IFDIR | 0o755), st_ctime=mtime, st_mtime=mtime, st_atime=mtime, st_nlink=2)
 
-      return value
+        return dict(st_mode=(S_IFREG), st_nlink=1, st_size=size, st_ctime=mtime, st_mtime=mtime, st_atime=time())
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
